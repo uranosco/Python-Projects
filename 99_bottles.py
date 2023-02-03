82b1930a20b8708c0c10e64af348388ee1f8b398
Banksy <banksy@duck.com>
2023-02-02 21:34:30 -0500

    new update

---
+++ 99_bottles.py
@@ -0,0 +1,8 @@
+
+num_bottles = 99
+while num_bottles > 0:
+     print(str(num_bottles) + " bottles of beer on the wall")
+     print(str(num_bottles) + " bottles of beer.")
+     print("Take one down, pass it around")
+     print(str(num_bottles - 1) + " bottles of beer on the wall!")
+     num_bottles = num_bottles - 1
---
+++ detention.py
@@ -0,0 +1,2 @@
+for x in range(100):
+    print("I will not use Snapchat in class")
